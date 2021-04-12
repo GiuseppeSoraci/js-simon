6002b721471e1db291b879db4568301472aae70f
GiuseppeSoraci <78480251+GiuseppeSoraci@users.noreply.github.com>
2021-04-12 20:00:13 +0200

    end

--- js/script.js
+++ js/script.js
@@ -1,20 +1,11 @@
-/*
-Descrizione
-Un alert() espone 5 numeri generati casualmente.
-Da li parte un timer di 30 secondi.
-Dopo 30 secondi l'utente deve inserire, uno alla volta, i numeri che ha visto precedentemente, tramite il prompt().
-Dopo che sono stati inseriti i 5 numeri, il software dice quanti e quali dei numeri da indovinare sono stati individuati.
-Consigli
-Pensate a quello che volete fare e scrivete dei commenti per aiutarvi con la logica
-Dividete in piccoli problemi la consegna.
-Individuate gli elementi di cui avete bisogno per realizzare il programma.
-*/
-
 $(document).ready(function () {
 
     // Refs
     var size = 5;
     var numberList = [];
+    var userList = [];
+    var numberGuessed = [];
+
 
     // Random Number
     while (numberList.length < size) {
@@ -25,16 +16,46 @@ $(document).ready(function () {
             numberList.push(number);
         }
     }
-    console.log(numberList);
 
     // Alert 
     alert("Read the following numbers carefully to try to win: " + numberList);
 
+    // Timer 
+    setTimeout(function () {
+
+        // Ask numbers 
+        while (userList.length < size) {
+            var userNumber = parseInt(prompt("Enter a number " + (userList.length + 1) + " of " + size));
+
+            // Validation
+            if (userNumber < 0 || userNumber > 99 || isNaN(userNumber)) {
+                var userNumber = parseInt(prompt("Sorry, value you entered is not valid. Enter a number " + (userList.length + 1) + " of " + size));
+            } else if (userList.includes(userNumber)) {
+                var userNumber = parseInt(prompt("Sorry, value you entered is not valid. Enter a number " + (userList.length + 1) + " of " + size));
+            } else (userList.push(userNumber));
+        }
+
+        // Guessed numbers
+        for (var i = 0; i < userList.length; i++) {
+
+            if (numberList.includes(userList[i])) {
+                numberGuessed.push(userList[i]);
+            }
+        }
+
+        // Feedback
+        // Random numbers
+        console.log("Numbers to guess were: " + numberList);
+        // User numbers
+        console.log("Numbers you entered are: " + userList);
+        // Result
+        console.log("You have guessed " + numberGuessed.length + " out of " + numberList.length);
+    }, 30000);
 });
 
 /**********
-FUNCTIONS
+UTILITIES
 **********/
 function getRandomNumber(min, max) {
     return Math.floor(Math.random() * (max - min + 1)) + min;
-}
+};
